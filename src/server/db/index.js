cf437cb8062e43737209bfa75f5c0437392a72ce
piralimic <pirali.michael@gmail.com>
2019-12-09 14:23:48 +0100

    update MONGODB_URI

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("debug", true); // enable logging collection methods + arguments to the console
 require("dotenv").config();
 
-const URI = process.env.MONGO_URI;
+const URI = process.env.MONGODB_URI;
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
